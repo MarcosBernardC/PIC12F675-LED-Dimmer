22fd92b71969003927b0b342d4b1a6c235dc50c4
MarcosBernardC <marcosbernardcalixto@gmail.com>
2026-02-18 16:01:33 -0500

    feat: ignorando directorio VideoEdit/. Firmware subido al PIC. Etapa 0 finalizada, led parpadeando con periodo de 500ms

--- src/main.c
+++ src/main.c
@@ -1,24 +1,22 @@
 #include <xc.h>
 
-// Configuración para el PIC12F675
-#pragma config FOSC = INTRCIO   // Oscilador interno (No necesitas cristal)
-#pragma config WDTE = OFF       // Watchdog apagado
-#pragma config PWRTE = ON       // Power-up Timer encendido
-#pragma config MCLRE = OFF      // GP3 es entrada digital, no Reset externo
+#pragma config FOSC = INTRCIO // Oscilador interno
+#pragma config WDTE = OFF     // Watchdog apagado
+#pragma config PWRTE = ON     // Power-up Timer encendido
+#pragma config MCLRE = OFF    // Pin GP3 como entrada (no reset)
 
-#define _XTAL_FREQ 4000000      // 4MHz interno
+#define _XTAL_FREQ 4000000 // 4MHz
 
 void main(void) {
-    // Configuración de pines
-    ANSEL  = 0x00;              // Todo digital
-    CMCON  = 0x07;              // Comparadores apagados
-    TRISIO = 0b001000;          // GP3 como entrada (siempre), el resto salidas (incluyendo GP2)
-    GPIO   = 0x00;              // Iniciar pines en bajo
+  ANSEL = 0x00;  // Digital I/O
+  CMCON = 0x07;  // Comparadores apagados
+  TRISIO = 0x00; // Todo como salida
+  GPIO = 0x00;   // Todo apagado
 
-    while(1) {
-        GPIO = 0b000100;        // Encender GP2 (Pin 5)
-        __delay_ms(500);
-        GPIO = 0b000000;        // Apagar GP2
-        __delay_ms(500);
-    }
+  while (1) {
+    GP2 = 1; // Enciende el LED azul
+    __delay_ms(500);
+    GP2 = 0; // Apaga el LED azul
+    __delay_ms(500);
+  }
 }
